90d30434a856452a7e7a739b4af01b15fcc9dacf
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-27 18:30:39 -0400

    added a pin form for accessing the shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import { useEffect, useState } from 'react'
-import { IdentificationForm } from './components/IdentificationForm'
+import { IdentificationForm } from './components/identification/IdentificationForm'
 import { ShoppingCart } from './components/ShoppingCart'
 const SCHOOL_ACRONYNM = window.location.pathname.split('/')[1] || 'demo'
 const SCHOOL_SOFT_API_URL = `https://www.schoolsoftpr.org/${SCHOOL_ACRONYNM}/cafeteria/self-service/requests/buttons.php`
 function App () {
   const [student, setStudent] = useState(null)
+  const [isConfirmed, setIsConfirmed] = useState(false)
   const [products, setProducts] = useState([])
   useEffect(() => {
     fetch(SCHOOL_SOFT_API_URL).then(data => data.json()).then(json => {
-      console.log({ json })
       setProducts(json)
+      console.log({ json })
     })
   }, [])
   function handleSetStudent (student) {
     setStudent(student)
   }
+  function handleSetConfirmed (confirmed) {
+    setIsConfirmed(confirmed)
+  }
 
-  if (student) {
+  if (isConfirmed) {
     return <ShoppingCart student={student} products={products}/>
   }
 
-  return <IdentificationForm setStudent={handleSetStudent}/>
+  return <IdentificationForm student={student} onPinConfirmed={handleSetConfirmed} setStudent={handleSetStudent}/>
 }
 
 export default App
