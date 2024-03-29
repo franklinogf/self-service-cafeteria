8a10cae1ee976c1aaf67e0c0ecf99c2acd5fe128
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-29 00:06:14 -0400

    new constantsfor the api

--- src/constants/school.js
+++ src/constants/school.js
@@ -1,2 +1,4 @@
 export const SCHOOL_ACRONYNM = window.location.pathname.split('/')[1] || 'cbl'
 export const SCHOOLSOFT_API_URL = `https://www.schoolsoftpr.org/${SCHOOL_ACRONYNM}/cafeteria/self-service/requests`
+export const SCHOOLSOFT_BUTTONS_API_URL = `${SCHOOLSOFT_API_URL}/buttons.php`
+export const SCHOOLSOFT_STUDENTS_API_URL = `${SCHOOLSOFT_API_URL}/students.php`
