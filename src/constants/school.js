0bd87282575aeeca556d011f384ec56ad5d8f018
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-04-03 21:21:54 -0400

    added payment api url constant

--- src/constants/school.js
+++ src/constants/school.js
@@ -2,3 +2,4 @@ export const SCHOOL_ACRONYNM = window.location.pathname.split('/')[1] || 'cbl'
 export const SCHOOLSOFT_API_URL = `https://www.schoolsoftpr.org/${SCHOOL_ACRONYNM}/cafeteria/self-service/requests`
 export const SCHOOLSOFT_BUTTONS_API_URL = `${SCHOOLSOFT_API_URL}/buttons.php`
 export const SCHOOLSOFT_STUDENTS_API_URL = `${SCHOOLSOFT_API_URL}/students.php`
+export const SCHOOLSOFT_PAYMENT_API_URL = `${SCHOOLSOFT_API_URL}/payment.php`
