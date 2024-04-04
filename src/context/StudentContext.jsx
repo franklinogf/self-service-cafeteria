f8dedbaad68286f107a35e1b2490fca6e07229c2
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-04-04 14:03:23 -0400

    student defatul value to null

--- src/context/StudentContext.jsx
+++ src/context/StudentContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react'
 export const StudentContext = createContext()
 
 export function StudentProvider ({ children }) {
-  const [student, setStudent] = useState(undefined)
+  const [student, setStudent] = useState(null)
   const [pinConfirmed, setPinConfirmed] = useState(false)
   return (
     <StudentContext.Provider value={{ student, setStudent, pinConfirmed, setPinConfirmed }}>
